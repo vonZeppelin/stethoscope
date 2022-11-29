b2dd7a4e6bbec71c3e6f216c8671e207de09f2bf
Leonid Bogdanov <leonidbogdanov86@gmail.com>
2022-11-30 10:13:58 +1100

    Update Ant Design and enable dark theme

--- ui/src/main/stethoscope/views.cljs
+++ ui/src/main/stethoscope/views.cljs
@@ -10,12 +10,12 @@
             (let [avatar-width 150
                   avatar (r/create-element
                           antd/Image
-                          #js{:src thumbnail 
+                          #js{:src thumbnail
                               :width avatar-width})
                   confirm-delete (r/as-element
                                   [:> antd/Popconfirm {:title "Delete this video?"
                                                        :placement "left"
-                                                       :onConfirm #(rf/dispatch [:delete-file id])}
+                                                       :on-confirm #(rf/dispatch [:delete-file id])}
                                    [:> antd/Button {:icon (r/create-element icons/DeleteOutlined)
                                                     :size "large"}]])]
               (r/as-element
@@ -37,38 +37,39 @@
     [:> antd/List {:bordered true
                    :dataSource (to-array @(rf/subscribe [:files]))
                    :loading @(rf/subscribe [:loading-files?])
-                   :renderItem file-renderer
-                   :rowKey :id}]))
+                   :render-item file-renderer
+                   :row-key :id}]))
 
 (defn queue-file-comp []
   (let [[form] (antd/Form.useForm)]
     [:> antd/Form {:form form
                    :name "queue-file"
                    :layout "inline"
-                   :onFinish (fn [fields]
-                               (form.resetFields)
-                               (rf/dispatch [:queue-file fields.link]))}
+                   :on-finish (fn [fields]
+                                (form.resetFields)
+                                (rf/dispatch [:queue-file fields.link]))}
      [:> antd/Form.Item {:label "YouTube link"
                          :name "link"
-                         :rules (clj->js [{:required true}
-                                          {:type "url"}])}
-      [:> antd/Input {:allowClear true}]]
+                         :rules [{:required true}
+                                 {:type "url"}]}
+      [:> antd/Input {:allow-clear true}]]
      [:> antd/Form.Item
-      [:> antd/Button {:htmlType "submit"
+      [:> antd/Button {:html-type "submit"
                        :icon (r/create-element icons/DownloadOutlined)
                        :type "primary"}
        "Download"]]]))
 
 (defn ui []
-  [:> antd/Layout
-   [:> antd/Layout.Header]
-   [:> antd/Layout.Content
-    [:> antd/Row
-     [:> antd/Col {:offset 5 :span 14}
-      [:f> queue-file-comp]
-      [list-files-comp]]]]
-   [:> antd/Layout.Footer {:style {:textAlign "center"}}
-    "2022 Stethoscope"]])
+  [:> antd/ConfigProvider {:theme {:algorithm antd/theme.darkAlgorithm}}
+   [:> antd/Layout
+    [:> antd/Layout.Header]
+    [:> antd/Layout.Content
+     [:> antd/Row
+      [:> antd/Col {:offset 5 :span 14}
+       [:f> queue-file-comp]
+       [list-files-comp]]]]
+    [:> antd/Layout.Footer {:style {:text-align "center"}}
+     "2022 Stethoscope"]]])
 
 (defn render []
   (rdom/render
