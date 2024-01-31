45f73d5e852c7cce5c9a35bd7cc99fbb30862270
Leonid Bogdanov <leonidbogdanov86@gmail.com>
2024-02-01 07:57:40 +1100

    Switch to feedgen lib

--- server/view/feed.py
+++ server/view/feed.py
@@ -1,10 +1,9 @@
 from datetime import datetime, timedelta, timezone
 from typing import AsyncIterable, Callable
 from urllib.parse import quote
-import warnings
 
 from aiohttp import web
-from podgen import Media, NotSupportedByItunesWarning, Podcast
+from feedgen.feed import FeedGenerator
 from sqlalchemy import null, select
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
@@ -36,29 +35,25 @@ class FeedView:
                 tzinfo=timezone.utc
             )
 
-        with warnings.catch_warnings(
-            category=NotSupportedByItunesWarning,
-            action="ignore"
-        ):
-            podcast = Podcast(
-                name="Leonid's Stethoscope",
-                description="Turns Youtube videos into podcast",
-                website=self._website,
-                explicit=False
+        podcast = FeedGenerator()
+        podcast.load_extension("podcast")
+        podcast.link(href=self._website, rel="self")
+        podcast.title("Leonid's Stethoscope")
+        podcast.description("Turns Youtube videos into podcast")
+
+        async with self._db_session() as db:
+            items = await db.stream_scalars(
+                select(Catalog)
+                .where(Catalog.parent_id == null())
+                .where(~Catalog.children.any())
+                .order_by(Catalog.created)
             )
-            async with self._db_session() as db:
-                items = await db.stream_scalars(
-                    select(Catalog)
-                    .where(Catalog.parent_id == null())
-                    .where(~Catalog.children.any())
-                    .order_by(Catalog.created)
-                )
-                await self._add_episodes(
-                    podcast, items, pub_date, media_link
-                )
+            await self._add_episodes(
+                podcast, items, pub_date, media_link
+            )
 
         return web.Response(
-            text=podcast.rss_str(minimize=True),
+            body=podcast.rss_str(),
             content_type="application/rss+xml"
         )
 
@@ -79,30 +74,25 @@ class FeedView:
             )
             return published + timedelta(hours=idx)
 
-        with warnings.catch_warnings(
-            category=NotSupportedByItunesWarning,
-            action="ignore"
-        ):
-            book_id = request.match_info["book_id"]
-            async with self._db_session() as db:
-                book = await db.get(Catalog, book_id)
-                podcast = Podcast(
-                    name=book.title,
-                    description=book.description,
-                    website=self._website,
-                    explicit=False
-                )
-                items = await db.stream_scalars(
-                    select(Catalog)
-                    .where(Catalog.parent_id == book_id)
-                    .order_by(Catalog.filename)
-                )
-                await self._add_episodes(
-                    podcast, items, pub_date, media_link
-                )
+        book_id = request.match_info["book_id"]
+        async with self._db_session() as db:
+            book = await db.get(Catalog, book_id)
+            podcast = FeedGenerator()
+            podcast.load_extension("podcast")
+            podcast.link(href=self._website, rel="self")
+            podcast.title(book.title)
+            podcast.description(book.description)
+            items = await db.stream_scalars(
+                select(Catalog)
+                .where(Catalog.parent_id == book_id)
+                .order_by(Catalog.filename)
+            )
+            await self._add_episodes(
+                podcast, items, pub_date, media_link
+            )
 
         return web.Response(
-            text=podcast.rss_str(minimize=True),
+            body=podcast.rss_str(),
             content_type="application/rss+xml"
         )
 
@@ -114,24 +104,24 @@ class FeedView:
 
     @staticmethod
     async def _add_episodes(
-            podcast: Podcast,
+            podcast: FeedGenerator,
             items: AsyncIterable[Catalog],
             pub_date_maker: Callable[[int, Catalog], datetime],
             media_link_maker: Callable[[Catalog], str]
     ):
         idx = 0
         async for i in items:
-            episode = podcast.add_episode()
-            episode.id = i.id
-            episode.title = i.title
-            episode.long_summary = i.description
-            episode.publication_date = pub_date_maker(idx, i)
-            episode.image = i.thumbnail_url
-            episode.media = Media(
+            episode = podcast.add_entry()
+            episode.id(i.id)
+            episode.title(i.title)
+            episode.description(i.description)
+            episode.published(pub_date_maker(idx, i))
+            episode.enclosure(
                 url=media_link_maker(i),
-                size=i.audio_size,
-                type=i.audio_type,
-                duration=timedelta(seconds=i.duration)
+                length=i.audio_size,
+                type=i.audio_type
             )
+            episode.podcast.itunes_duration(i.duration)
+            # TODO episode.podcast.itunes_image(i.thumbnail_url)
 
             idx += 1
